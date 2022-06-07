116eaca735b34fa699aca5738611122cb3925498
Paul Merizalde <merizaldepaul1999@gmail.com>
2022-06-06 20:08:15 -0500

    Taller 7, cohesion, estereotipos Service & Repository

--- src/main/java/com/uce/edu/demo/ProyectoU1PmApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU1PmApplication.java
@@ -1,35 +1,45 @@
 package com.uce.edu.demo;
 
-import java.time.LocalDateTime;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.consultorio.CitaMedica2;
+import com.uce.edu.demo.modelo.Estudiante;
+import com.uce.edu.demo.service.IEstudianteService;
 
 @SpringBootApplication
 public class ProyectoU1PmApplication implements CommandLineRunner {
 
-	// 1) DI por atributo
-//	@Autowired
-//	private CitaMedica cita;
-	
 	@Autowired
-	private CitaMedica2 cita;
-
+	private IEstudianteService estudianteService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU1PmApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("Mi primer proyecto con Spring Framework");
-
-		String respuesta = cita.agendar(LocalDateTime.now(), "Javier", "Terán", 32, "Quito", "Pepito", 19);
-		System.out.println(respuesta);
+		
+		Estudiante e = new Estudiante();
+		e.setNombre("Paul");
+		e.setApellido("Merizalde");
+		e.setCedula("1728189521");
+		this.estudianteService.ingresarEstudiante(e);
+		
+		Estudiante e1 = new Estudiante();
+		e1.setNombre("Pepito");
+		e1.setApellido("Perez");
+		e1.setCedula("1721436554");
+		this.estudianteService.ingresarEstudiante(e1);
+		
+		//Usar los 3 metodos restantes
+		e.setCedula("1728189522");
+		this.estudianteService.actualizarEstudiante(e);
+		
+		this.estudianteService.buscarPorApellido("Merizalde");
+		
+		this.estudianteService.borrarEstudiante("1728189522");
 	}
 
 }
